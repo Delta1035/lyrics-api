183ea34f21a9b51951cd5c35430fe52f3d4040db
delta <528491526@qq.com>
2023-07-12 23:53:58 +0800

    feat✨: 增加递归遍历文件目录

--- scan.js
+++ scan.js
@@ -1,18 +1,30 @@
 const path = require("path");
 const config = require("./config");
 const fs = require("fs");
-module.exports = function scanLyrics() {
-  const lyricsPath = path.join(__dirname, config.lyricsPath);
-  fs.stat(lyricsPath, (err, stats) => {});
-  const lyrics = fs.readdirSync(lyricsPath).map((_path) => {
-    const [song, suffix] = _path.split(".lri");
-    const [singer, title] = song.split(" - ");
-    return {
-      title: (title ?? "").trim(),
-      singer: (singer ?? "").trim(),
-      path: path.join(__dirname, config.lyricsPath, _path),
-    };
-  });
 
-  fs.writeFileSync(config.lyricsCacheFileName, JSON.stringify(lyrics));
-};
+const pathCache = [];
+const lyrics = [];
+function readFilesWithLriExtension(directoryPath) {
+  const items = fs.readdirSync(directoryPath);
+  items.forEach((item) => {
+    const itemPath = path.join(directoryPath, item);
+    const isDirectory = fs.statSync(itemPath).isDirectory();
+    if (isDirectory) {
+      readFilesWithLriExtension(itemPath);
+    } else {
+      if (path.extname(item) === ".lrc") {
+        pathCache.push(path.join(directoryPath, item));
+        const [song, suffix] = item.split(".lri");
+        const [singer, title] = song.split(" - ");
+        lyrics.push({
+          title: (title ?? "").trim(),
+          singer: (singer ?? "").trim(),
+          path: path.join(directoryPath, item),
+        });
+      }
+    }
+  });
+}
+const lyricsPath = path.join(__dirname, config.lyricsPath);
+readFilesWithLriExtension(lyricsPath);
+fs.writeFileSync(config.lyricsCacheFileName, JSON.stringify(lyrics));
